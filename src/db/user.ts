270c1e1e4e5695a022d6b8304c240004318e1a77
HologramEngineer <adrianselecky21@gmail.com>
2025-01-19 18:17:28 +0100

    - created initial user database model with required columns

---
+++ src/db/user.ts
@@ -0,0 +1,53 @@
+import {
+    Sequelize,
+    DataTypes,
+} from "sequelize";
+import {DatabaseModel} from "../types/db";
+
+import {ROLE} from "../utils/enums";
+
+export class UserModel extends DatabaseModel {
+    id: number
+    name: string
+    surname: string
+    nickName: string
+    email: string
+    age: number
+    role: ROLE
+}
+
+export default (sequelize: Sequelize) => {
+    UserModel.init({
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(200)
+        },
+        surname: {
+            type: DataTypes.STRING(200)
+        },
+        nickName: {
+            type: DataTypes.STRING(200)
+        },
+        email: {
+            type: DataTypes.STRING(200)
+        },
+        age: {
+            type: DataTypes.INTEGER,
+        },
+        role: {
+            type: DataTypes.ENUM(...Object.values(ROLE))
+        }
+    }, {
+        paranoid: true,
+        timestamps: true,
+        sequelize,
+        modelName: 'user'
+    })
+
+    return UserModel
+}
