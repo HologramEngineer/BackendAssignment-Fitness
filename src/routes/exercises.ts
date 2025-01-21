970ee472544d5b36d7b53c407c0e77cb82a73046
HologramEngineer <adrianselecky21@gmail.com>
2025-01-22 00:45:05 +0100

    - [bonus task 1] added filter by program and fulltext search to exercise get request

--- src/routes/exercises.ts
+++ src/routes/exercises.ts
@@ -10,129 +10,148 @@ import {FindOptions} from "sequelize";
 const router: Router = Router()
 
 const {
-    Exercise,
-    Program
+	Exercise,
+	Program
 } = models
 
 export default () => {
-    router.get('/', async (_req: Request, res: Response, _next: NextFunction) => {
-        let options:FindOptions = {
-            include: [{
-                model: Program,
-                as: 'program'
-            }]
-        }
+	router.get('/', async (_req: Request, res: Response, _next: NextFunction) => {
+		let options: FindOptions = {
+			include: [{
+				model: Program,
+				as: 'program'
+			}]
+		}
 
-        if (_req.query.page && _req.query.limit)
-        {
-            console.log('Doing paginated search with limit ' + _req.query.limit + ' starting at page ' + _req.query.page)
+		if (_req.query.page && _req.query.limit) {
+			console.log('Doing paginated search with limit ' + _req.query.limit + ' starting at page ' + _req.query.page)
 
-            const limit = parseInt(<string>_req.query.limit)
-            const offset = (parseInt(<string>_req.query.page) - 1) * limit
+			const limit = parseInt(<string>_req.query.limit)
+			const offset = (parseInt(<string>_req.query.page) - 1) * limit
 
-            options.limit = limit
-            options.offset = offset
+			options.limit = limit
+			options.offset = offset
 
-        }
+		}
 
-        const exercises = await Exercise.findAll(options)
+		if (_req.query.programID) {
+			if (_req.query.search) {
+				console.log('Filtering exercise by name: ' + _req.query.search + ' and program: ' + _req.query.programID)
 
-        return res.json({
-            data: exercises,
-            message: 'List of exercises'
-        })
-    })
+				options.where = {
+					name: _req.query.search,
+					programID: _req.query.programID
+				}
+			} else {
+				console.log('Filtering exercise search by program: ' + _req.query.programID)
 
-    router.post('/', passport.authenticate('jwt', {session: false}),
-        async (_req: Request, res: Response, _next: NextFunction) => {
-            const user = _req.user as UserModel
+				options.where = {programID: _req.query.programID}
+			}
+		} else if (_req.query.search) {
+			console.log('Filtering exercise by name: ' + _req.query.search)
 
-            if (user.role != ROLE.ADMIN)
-                return res.status(403).send('Creating, updating or deleting exercises requires ADMIN privileges.')
+			options.where = {name: _req.query.search}
+		}
 
-            let diff: EXERCISE_DIFFICULTY
 
-            if (_req.body.difficulty != undefined) {
-                const diffString = (_req.body.difficulty as string).toUpperCase() as keyof typeof EXERCISE_DIFFICULTY
-                diff = EXERCISE_DIFFICULTY[diffString]
-            }
+		const exercises = await Exercise.findAll(options)
 
-            try {
-                const newExercise = await Exercise.create({
-                    name: _req.body.name,
-                    difficulty: diff,
-                    programID: _req.body.programID
-                })
+		return res.json({
+			data: exercises,
+			message: 'List of exercises'
+		})
+	})
 
-                if (newExercise == undefined) {
-                    return res.status(400).send('Something went wrong creating exercise.')
-                }
+	router.post('/', passport.authenticate('jwt', {session: false}),
+		async (_req: Request, res: Response, _next: NextFunction) => {
+			const user = _req.user as UserModel
 
-                return res.status(200).send('Exercise created successfully.')
-            } catch (error) {
-                return res.status(400).send(error)
-            }
-        })
+			if (user.role != ROLE.ADMIN)
+				return res.status(403).send('Creating, updating or deleting exercises requires ADMIN privileges.')
 
-    router.patch('/', passport.authenticate('jwt', {session: false}),
-        async (_req: Request, res: Response, _next: NextFunction) => {
-            const user = _req.user as UserModel
+			let diff: EXERCISE_DIFFICULTY
 
-            if (user.role != ROLE.ADMIN)
-                return res.status(403).send('Creating, updating or deleting exercises requires ADMIN privileges.')
+			if (_req.body.difficulty != undefined) {
+				const diffString = (_req.body.difficulty as string).toUpperCase() as keyof typeof EXERCISE_DIFFICULTY
+				diff = EXERCISE_DIFFICULTY[diffString]
+			}
 
-            try {
-                let newExercise = await Exercise.findOne({where: {id:_req.body.id}}) as ExerciseModel
-                let update = false
+			try {
+				const newExercise = await Exercise.create({
+					name: _req.body.name,
+					difficulty: diff,
+					programID: _req.body.programID
+				})
 
-                if (newExercise == undefined) {
-                    return res.status(400).send('Exercise with id ' + _req.body.id + ' could not be found.')
-                }
+				if (newExercise == undefined) {
+					return res.status(400).send('Something went wrong creating exercise.')
+				}
 
-                if (_req.body.name != undefined) {
-                    newExercise.name = _req.body.name
-                    update = true
-                }
+				return res.status(200).send('Exercise created successfully.')
+			} catch (error) {
+				return res.status(400).send(error)
+			}
+		})
 
-                if (_req.body.difficulty != undefined) {
-                    let diffString = (_req.body.difficulty as string).toUpperCase() as keyof typeof EXERCISE_DIFFICULTY
-                    newExercise.difficulty = EXERCISE_DIFFICULTY[diffString]
-                    update = true
-                }
+	router.patch('/', passport.authenticate('jwt', {session: false}),
+		async (_req: Request, res: Response, _next: NextFunction) => {
+			const user = _req.user as UserModel
 
-                // note: foreign key can't be updated this way because of model structure, and I'm not convinced it is needed
-                //          admin should remove affected exercise completely and then create new one with proper programID
+			if (user.role != ROLE.ADMIN)
+				return res.status(403).send('Creating, updating or deleting exercises requires ADMIN privileges.')
 
-                if (!update)
-                    return res.status(200).send('Nothing to update.')
+			try {
+				let newExercise = await Exercise.findOne({where: {id: _req.body.id}}) as ExerciseModel
+				let update = false
 
-                await newExercise.save()
+				if (newExercise == undefined) {
+					return res.status(400).send('Exercise with id ' + _req.body.id + ' could not be found.')
+				}
 
-                return res.status(200).send('Exercise with id ' + _req.body.id + ' updated successfully.')
-            } catch (error) {
-                return res.status(400).send(error)
-            }
-        })
+				if (_req.body.name != undefined) {
+					newExercise.name = _req.body.name
+					update = true
+				}
 
-    router.delete('/', passport.authenticate('jwt', {session: false}),
-        async (_req: Request, res: Response, _next: NextFunction) => {
-            const user = _req.user as UserModel
+				if (_req.body.difficulty != undefined) {
+					let diffString = (_req.body.difficulty as string).toUpperCase() as keyof typeof EXERCISE_DIFFICULTY
+					newExercise.difficulty = EXERCISE_DIFFICULTY[diffString]
+					update = true
+				}
 
-            if (user.role != ROLE.ADMIN)
-                return res.status(403).send('Creating, updating or deleting exercises requires ADMIN privileges.')
+				// note: foreign key can't be updated this way because of model structure, and I'm not convinced it is needed
+				//          admin should remove affected exercise completely and then create new one with proper programID
 
-            try {
-                const rowsUpdated = await Exercise.destroy({where: {id: _req.body.id}})
+				if (!update)
+					return res.status(200).send('Nothing to update.')
 
-                if (rowsUpdated>0)
-                    return res.status(200).send('Exercise with id ' + _req.body.id + ' deleted successfully.')
-                else
-                    return res.status(200).send('Exercise with id ' + _req.body.id + ' was not deleted. ' +
-                        'Please check if it exists.')
-            } catch (error) {
-                return res.status(400).send(error)
-            }
-        })
+				await newExercise.save()
 
-    return router
+				return res.status(200).send('Exercise with id ' + _req.body.id + ' updated successfully.')
+			} catch (error) {
+				return res.status(400).send(error)
+			}
+		})
+
+	router.delete('/', passport.authenticate('jwt', {session: false}),
+		async (_req: Request, res: Response, _next: NextFunction) => {
+			const user = _req.user as UserModel
+
+			if (user.role != ROLE.ADMIN)
+				return res.status(403).send('Creating, updating or deleting exercises requires ADMIN privileges.')
+
+			try {
+				const rowsUpdated = await Exercise.destroy({where: {id: _req.body.id}})
+
+				if (rowsUpdated > 0)
+					return res.status(200).send('Exercise with id ' + _req.body.id + ' deleted successfully.')
+				else
+					return res.status(200).send('Exercise with id ' + _req.body.id + ' was not deleted. ' +
+						'Please check if it exists.')
+			} catch (error) {
+				return res.status(400).send(error)
+			}
+		})
+
+	return router
 }
