b9e263e2672aedd32fd00c6beba4f173f64fa60f
HologramEngineer <adrianselecky21@gmail.com>
2025-01-22 00:19:29 +0100

    - added api to remove tracked exercise

--- src/controllers/trackingController.ts
+++ src/controllers/trackingController.ts
@@ -22,7 +22,7 @@ exports.getTrackedExercises = async (_req: Request, res: Response, _next: NextFu
 
 	return res.json({
 		data: trackedExercises,
-		message: 'List of exercises'
+		message: 'List of tracked exercises'
 	})
 }
 
@@ -42,6 +42,27 @@ exports.postTrackedExercise = async (_req: Request, res: Response, _next: NextFu
 		}
 
 		return res.status(200).send('Exercise created successfully.')
+	} catch (error) {
+		return res.status(400).send(error)
+	}
+}
+
+exports.removeTrackedExercise = async (_req: Request, res: Response, _next: NextFunction) => {
+	let user = _req.user as UserModel
+
+	try {
+		const rowsUpdated = await ExerciseHistory.destroy({
+			where: {
+				id: _req.body.id,
+				userID: user.id
+			}
+		})
+
+		if (rowsUpdated > 0)
+			return res.status(200).send('Tracked exercise with id ' + _req.body.id + ' deleted successfully.')
+		else
+			return res.status(200).send('Tracked exercise with id ' + _req.body.id + ' was not deleted. ' +
+				'Please check if it exists and if it belongs to you.')
 	} catch (error) {
 		return res.status(400).send(error)
 	}
--- src/routes/tracking.ts
+++ src/routes/tracking.ts
@@ -9,6 +9,7 @@ const trackingController = require('../controllers/trackingController')
 export default () => {
 	router.get('/', passport.authenticate('jwt', {session: false}), trackingController.getTrackedExercises)
 	router.post('/', passport.authenticate('jwt', {session: false}),trackingController.postTrackedExercise)
+	router.delete('/', passport.authenticate('jwt', {session: false}),trackingController.removeTrackedExercise)
 
 	return router
 }
